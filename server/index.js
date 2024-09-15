7a6bafbd8221aac3acdbac3d8017365f98afe97b
Harshwardhan Choudhary <choudharyharshwardhan231@gmail.com>
2024-09-16 00:21:46 +0530

    started

---
+++ server/index.js
@@ -0,0 +1,27 @@
+const express = require("express")
+const cors = require("cors")
+const mongoose = require("mongoose")
+const userRoute = require("./Routes/userRoute")
+
+const app = express()
+require("dotenv").config()
+
+app.use(express.json())
+app.use(cors())
+
+app.get("/", (req, res) =>{
+    res.send("Welcome to our chat app APIs...")
+})
+
+const port = process.env.PORT || 5000
+const uri = process.env.ATLAS_URI;
+
+app.listen(port, (req, res) =>{
+    console.log(`server running on port: ${port}`)
+})
+
+mongoose.connect(uri, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true
+}).then(() => console.log("MongoDB connection established")).catch((error) => console.log("MongoDB connection fail: ", error.message))
+
